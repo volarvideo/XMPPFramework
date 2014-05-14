4261783630e1880fbcb94ccf312affffbf4cd59c
Benjamin Kant <benkant@gmail.com>
2014-05-14 10:26:01 -0400

    Removed send/receive logging from XMPPStream class.

--- Core/XMPPStream.m
+++ Core/XMPPStream.m
@@ -20,7 +20,7 @@
 
 // Log levels: off, error, warn, info, verbose
 #if DEBUG
-  static const int xmppLogLevel = XMPP_LOG_LEVEL_INFO | XMPP_LOG_FLAG_SEND_RECV; // | XMPP_LOG_FLAG_TRACE;
+  static const int xmppLogLevel = XMPP_LOG_LEVEL_INFO; //| XMPP_LOG_FLAG_SEND_RECV; (this logs all messages send and recieved)
 #else
   static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 #endif
